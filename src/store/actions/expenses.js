5a6a8e814cd8c9836e266f63529ecd9cedd1cca5
Marcos Guilherme Goulart Moraes Campos <61772359+Markimg22@users.noreply.github.com>
2020-11-23 10:42:45 -0400

    details and snapshots

--- src/store/actions/expenses.js
+++ src/store/actions/expenses.js
@@ -9,19 +9,11 @@ export const addExpense = (expense) => ({
 export const startAddExpense = (expenseData = {}) => {
   return (dispatch, getState) => {
     const uid = getState().auth.uid;
-    const {
-      description = '',
-      note = '',
-      amount = 0,
-      createdAt = 0
-    } = expenseData;
+    const { description = '', note = '', amount = 0, createdAt = 0 } = expenseData;
     const expense = { description, note, amount, createdAt };
 
     return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
-      dispatch(addExpense({
-        id: ref.key,
-        ...expense
-      }));
+      dispatch(addExpense({ id: ref.key, ...expense }));
     });
   };
 };
@@ -68,14 +60,9 @@ export const startSetExpenses = () => {
     const uid = getState().auth.uid;
     return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
       const expenses = [];
-
       snapshot.forEach((childSnapshot) => {
-        expenses.push({
-          id: childSnapshot.key,
-          ...childSnapshot.val()
-        });
+        expenses.push({ id: childSnapshot.key, ...childSnapshot.val() });
       });
-
       dispatch(setExpenses(expenses));
     });
   };
